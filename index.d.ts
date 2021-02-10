0da5ae309460f82a71f2d71fff350f7318326a18
Satoshi Nakagawa <ghnacker@gmail.com>
2021-02-10 16:04:39 +0900

    scoped index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module 'connected-react-router' {
+declare module '@ghnacker/connected-react-router' {
   import * as React from 'react';
   import { Middleware, Reducer } from 'redux';
   import { ReactReduxContextValue } from 'react-redux';
